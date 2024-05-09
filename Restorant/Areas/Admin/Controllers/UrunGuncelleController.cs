3f020b08169364b16110c5fbadaa61b0b177e022
buseaydinn <buseeaydnn01@gmail.com>
2024-05-09 17:21:29 +0300

    ürün güncelleme yaptım

---
+++ Restorant/Areas/Admin/Controllers/UrunGuncelleController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Restorant.Data;
+using Restorant.Models;
+
+namespace Restorant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UrunGuncelleController : Controller
+    {
+
+        private readonly IdentityDataContext _context;
+
+        public UrunGuncelleController(IdentityDataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UrunGuncelle(int id)
+        {
+            ViewBag.Kategori = _context.Kategoriler.ToList();
+
+
+            // IdentityDataContext.Personeller özelliğinden belirli bir personeli alın.
+            var urun = _context.Urunler.FirstOrDefault(p => p.Id == id);
+
+            // Eğer personel bulunamazsa 404 hatası döndürün.
+            if (urun == null)
+            {
+                return NotFound();
+            }
+
+            // PersonelGuncelleModel oluştururken doğru personel nesnesini kullanın.
+            return View(urun);
+        }
+        [HttpPost]
+        public IActionResult UrunGuncelle(Urun model)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(model); // Geçersiz model ise formu tekrar gösterin.
+            }
+
+            var urun = _context.Urunler.FirstOrDefault(x => x.Id == model.Id);
+            if (urun == null)
+            {
+                return NotFound(); // Eğer personel bulunamazsa 404 hatası döndürün.
+            }
+
+            // Önceki soruguyu untracked yani takipsiz yapma
+            var entry = _context.Entry(urun);
+            entry.State = EntityState.Detached;
+            _context.Update(model); // Güncellenmiş personel bilgilerini kaydedin.
+            _context.SaveChanges();
+
+            return RedirectToAction("UrunListele", "Urun"); // İşlem başarılıysa ana sayfaya yönlendirin.
+        }
+
+    }
+}
+
