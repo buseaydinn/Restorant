fa30d10d95d089e7b498cb2522ed24135ce98029
buseaydinn <buseeaydnn01@gmail.com>
2024-05-09 15:36:04 +0300

    Masa Güncelle yapıldı

---
+++ Restorant/Areas/Admin/Controllers/MalzemeGuncelleController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Restorant.Data;
+using Restorant.Models;
+
+namespace Restorant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MalzemeGuncelleController : Controller
+    {
+
+        private readonly IdentityDataContext _context;
+
+        public MalzemeGuncelleController(IdentityDataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MalzemeGuncelle(int id)
+        {
+            ViewBag.Malzemeler = _context.Malzemeler.ToList();
+
+            // IdentityDataContext.Personeller özelliğinden belirli bir personeli alın.
+            var malzeme = _context.Malzemeler.FirstOrDefault(p => p.Id == id);
+
+            // Eğer personel bulunamazsa 404 hatası döndürün.
+            if (malzeme == null)
+            {
+                return NotFound();
+            }
+
+            // PersonelGuncelleModel oluştururken doğru personel nesnesini kullanın.
+            return View(malzeme);
+        }
+        [HttpPost]
+        public IActionResult MalzemeGuncelle(Malzeme model)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(model); // Geçersiz model ise formu tekrar gösterin.
+            }
+
+            var malzeme = _context.Malzemeler.FirstOrDefault(x => x.Id == model.Id);
+            if (malzeme == null)
+            {
+                return NotFound(); // Eğer personel bulunamazsa 404 hatası döndürün.
+            }
+
+            // Önceki soruguyu untracked yani takipsiz yapma
+            var entry = _context.Entry(malzeme);
+            entry.State = EntityState.Detached;
+            _context.Update(model); // Güncellenmiş personel bilgilerini kaydedin.
+            _context.SaveChanges();
+
+            return RedirectToAction("MalzemeListele", "Malzeme"); // İşlem başarılıysa ana sayfaya yönlendirin.
+        }
+    }
+}
+
---
+++ Restorant/Areas/Admin/Controllers/MasaGuncelleController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Restorant.Data;
+using Restorant.Models;
+
+namespace Restorant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MasaGuncelleController : Controller
+    {
+
+        private readonly IdentityDataContext _context;
+
+        public MasaGuncelleController(IdentityDataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MasaGuncelle(int id)
+        {
+            ViewBag.Kategoriler = _context.Kategoriler.ToList();
+
+            // IdentityDataContext.Personeller özelliğinden belirli bir personeli alın.
+            var masa = _context.Masalar.FirstOrDefault(p => p.Id == id);
+
+            // Eğer personel bulunamazsa 404 hatası döndürün.
+            if (masa == null)
+            {
+                return NotFound();
+            }
+
+            // PersonelGuncelleModel oluştururken doğru personel nesnesini kullanın.
+            return View(masa);
+        }
+        [HttpPost]
+        public IActionResult MasaGuncelle(Masa model)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(model); // Geçersiz model ise formu tekrar gösterin.
+            }
+
+            var masa = _context.Masalar.FirstOrDefault(x => x.Id == model.Id);
+            if (masa == null)
+            {
+                return NotFound(); // Eğer personel bulunamazsa 404 hatası döndürün.
+            }
+
+            // Önceki soruguyu untracked yani takipsiz yapma
+            var entry = _context.Entry(masa);
+            entry.State = EntityState.Detached;
+            _context.Update(model); // Güncellenmiş personel bilgilerini kaydedin.
+            _context.SaveChanges();
+
+            return RedirectToAction("MasaListele", "Masa"); // İşlem başarılıysa ana sayfaya yönlendirin.
+        }
+    }
+}
+
+   
